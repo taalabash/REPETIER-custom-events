a45973093188044d28fd42a80e1f137245e35e9b
taalabash <taalabash@users.noreply.github.com>
2017-06-15 18:00:01 +0300

    Add files via upload

--- CustomEvents.h
+++ CustomEvents.h
@@ -28,29 +28,13 @@ extern bool Custom_GCode(GCode *com);
 extern bool Custom_MCode(GCode *com); 
 
 
-//static float memoryX1;
-//static float memoryX2;
-//static float memoryX3;
-//
-//static float memoryY1;
-//static float memoryY2;
-//static float memoryY3;
-//
-//static float memoryZ1;
-//static float memoryZ2;
-//static float memoryZ3;
+
 
 #define epr_memoryX1        400
-#define epr_memoryX2        401
-#define epr_memoryX3        402
+#define epr_memoryY1        401
+#define epr_memoryZ1        402
 
-#define epr_memoryY1        403
-#define epr_memoryY2        404
-#define epr_memoryY3        405
 
-#define epr_memoryZ1        406
-#define epr_memoryZ2        407
-#define epr_memoryZ3        408
 
 
 
--- CustomEventsImpl.h
+++ CustomEventsImpl.h
@@ -47,16 +47,9 @@
 //#### MCode addition/replacement
 //#########################################################################################
 float memoryX1 = IGNORE_COORDINATE;
-float memoryX2 = IGNORE_COORDINATE;
-float memoryX3 = IGNORE_COORDINATE;
-
 float memoryY1 = IGNORE_COORDINATE;
-float memoryY2 = IGNORE_COORDINATE;
-float memoryY3 = IGNORE_COORDINATE;
-
 float memoryZ1 = IGNORE_COORDINATE;
-float memoryZ2 = IGNORE_COORDINATE;
-float memoryZ3 = IGNORE_COORDINATE;
+
 
 
 
@@ -71,7 +64,7 @@ bool Custom_MCode(GCode *com)
        if(com->hasP()){
         switch(com->P){
          
-		 case 1:
+		     case 1:
           Commands::waitUntilEndOfAllMoves();
           Printer::updateCurrentPosition(false);
           Printer::realPosition(memoryX1, memoryY1, memoryZ1);
@@ -80,42 +73,17 @@ bool Custom_MCode(GCode *com)
           HAL::eprSetFloat(epr_memoryY1,memoryY1);
           HAL::eprSetFloat(epr_memoryZ1,memoryZ1);
 
-
-          Com::config(PSTR("SAVED X1: "),HAL::eprGetFloat(epr_memoryX1));
-          Com::config(PSTR("SAVED Y1: "),HAL::eprGetFloat(epr_memoryY1));
-          Com::config(PSTR("SAVED Z1: "),HAL::eprGetFloat(epr_memoryZ1));
-
-         break;
-
-         case 2:
-          Commands::waitUntilEndOfAllMoves();
-          Printer::updateCurrentPosition(false);
-          Printer::realPosition(memoryX2, memoryY2, memoryZ2);
-
-          HAL::eprSetFloat(epr_memoryX2,memoryX2);
-          HAL::eprSetFloat(epr_memoryY2,memoryY2);
-          HAL::eprSetFloat(epr_memoryZ2,memoryZ2);
-
-          Com::config(PSTR("SAVED X2: "),HAL::eprGetFloat(epr_memoryX1));
-          Com::config(PSTR("SAVED Y2: "),HAL::eprGetFloat(epr_memoryY2));
-          Com::config(PSTR("SAVED Z2: "),HAL::eprGetFloat(epr_memoryZ2));
-
-         break;
-
-         case 3:
-          Commands::waitUntilEndOfAllMoves();
-          Printer::updateCurrentPosition(false);
-          Printer::realPosition(memoryX3, memoryY3, memoryZ3);
+          Com::printF(PSTR("Saved to position 1: X:"),HAL::eprGetFloat(memoryX1));
+          Com::printF(PSTR("  Y:"),HAL::eprGetFloat(memoryY1));
+          Com::printFLN(PSTR("  Z:"),HAL::eprGetFloat(memoryZ1));   
           
-          HAL::eprSetFloat(epr_memoryX3,memoryX3);
-          HAL::eprSetFloat(epr_memoryY3,memoryY3);
-          HAL::eprSetFloat(epr_memoryZ3,memoryZ3);
+          Com::printF(PSTR("Saved to position 1: X:"),HAL::eprGetFloat(epr_memoryX1));
+          Com::printF(PSTR("  Y:"),HAL::eprGetFloat(epr_memoryY1));
+          Com::printFLN(PSTR("  Z:"),HAL::eprGetFloat(epr_memoryZ1));   
+        
+         break;
 
-          Com::config(PSTR("SAVED X3: "),HAL::eprGetFloat(epr_memoryX3));
-          Com::config(PSTR("SAVED Y3: "),HAL::eprGetFloat(epr_memoryY3));
-          Com::config(PSTR("SAVED Z3: "),HAL::eprGetFloat(epr_memoryZ3));
-
-         break;}
+              }
          }
          #else Com::printErrorF(Com::tNoEEPROMSupport);
          #endif
@@ -124,7 +92,7 @@ bool Custom_MCode(GCode *com)
 
 //  --------------------------------------------------------------------
 
-     case 481:
+     case 481:  //M481
       #if EEPROM_MODE !=0
        if(com->hasP()){
         switch(com->P){
@@ -135,28 +103,29 @@ bool Custom_MCode(GCode *com)
 
          break;
 
-         case 2:        
-            Com::config(PSTR("MEMORY X2:"),HAL::eprGetFloat(epr_memoryX2));
-            Com::config(PSTR("MEMORY Y2:"),HAL::eprGetFloat(epr_memoryY2));
-            Com::config(PSTR("MEMORY Z2:"),HAL::eprGetFloat(epr_memoryZ2));
-
-         break;
-
-         case 3:      
-            Com::config(PSTR("MEMORY X3:"),HAL::eprGetFloat(epr_memoryX3));
-            Com::config(PSTR("MEMORY Y3:"),HAL::eprGetFloat(epr_memoryY3));
-            Com::config(PSTR("MEMORY Z3:"),HAL::eprGetFloat(epr_memoryZ3));
-
-         break;}
+            }
          }
          #else Com::printErrorF(Com::tNoEEPROMSupport);
          #endif
 
-    break;
+    break;  //end 481
 
 //  -------------------------------------------------------------------
-// 
-  
+    case 482:   //M482
+    #if EEPROM_MODE !=0
+       if(com->hasP()){
+        switch(com->P){
+         case 1:
+         
+            Printer::moveToReal(HAL::eprGetFloat(epr_memoryX1),HAL::eprGetFloat(epr_memoryY1),HAL::eprGetFloat(epr_memoryZ1),IGNORE_COORDINATE,(com->hasF() ? com->F : Printer::feedrate));
+
+         break;
+
+            }
+         }
+         #else Com::printErrorF(Com::tNoEEPROMSupport);
+         #endif
+      break;  //end 482
   
   default:
      return false;
